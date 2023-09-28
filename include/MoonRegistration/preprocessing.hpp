8038b86b4de2ca684937962017184aaa6fd15482
Gavin1937 <gyh1837771475gyh@gmail.com>
2023-09-27 20:27:39 -0700

    impl preprocessing funcs

--- include/MoonRegistration/preprocessing.hpp
+++ include/MoonRegistration/preprocessing.hpp
@@ -1,12 +1,105 @@
 #pragma once
 
 #include <opencv2/core/mat.hpp>
+#include <opencv2/imgproc.hpp>
 #include "../include/MoonRegistration/macros.h"
 
 namespace mr
 {
 
-EXPORT_SYMBOL void resize_with_aspect_ratio(cv::Mat image, int width, int height, int longer_side_val, int inter);
+// original algorithm
+// https://stackoverflow.com/a/58126805
+// 
+// Parameters:
+//   - image_in: input image
+//   - image_out: output image
+//   - ratio_out: output resize ratio
+//   - width: resize image to this width (-1 to ignore)
+//   - height: resize image to this height (-1 to ignore)
+//   - longer_side_val: resize image's longer side to this value (-1 to ignore)
+//   - inter: interpolation for cv::resize()
+EXPORT_SYMBOL void resize_with_aspect_ratio(
+    const cv::Mat& image_in,
+    cv::Mat& image_out,
+    float& ratio_out,
+    int width           = -1,
+    int height          = -1,
+    int longer_side_val = -1,
+    int inter           = cv::INTER_AREA
+);
 
+// original algorithm
+// https://stackoverflow.com/a/50053219
+// 
+// Parameters:
+//   - image_in: input image
+//   - image_out: output image
+//   - brightness: set image brightness value (-127 to 127) (default 0)
+//   - contrast: set image contrast value (-127 to 127) (default 0)
+EXPORT_SYMBOL void apply_brightness_contrast(
+    const cv::Mat& image_in,
+    cv::Mat& image_out,
+    int brightness      = 0,
+    int contrast        = 0
+);
+
+// Assuming all pixel values in image_in are black(0) / white(255)
+// sum up all the pixels in given image, and then
+// calculate its mean. The return value represents
+// how many white pixels in the image, how bright it is.
+// 
+// Parameters:
+//   - image_in: input image
+// 
+// Returns:
+//   - float between 0 to 1
+EXPORT_SYMBOL float calc_img_brightness_perc(
+    const cv::Mat& image_in
+);
+
+// Assuming all pixel values in image_in are black(0) / white(255)
+// sum up all the pixels in given circle, and then
+// calculate its mean. The return value represents
+// how many white pixels in the image, how bright it is.
+// 
+// part of algorithm
+// https://stackoverflow.com/a/74813748
+// 
+// Parameters:
+//   - image_in: input image
+//   - center_x: circle center x
+//   - center_y: circle center y
+//   - radius: circle radius
+// 
+// Returns:
+//   - float between 0 to 1
+EXPORT_SYMBOL float calc_circle_brightness_perc(
+    const cv::Mat& image_in,
+    int center_x,
+    int center_y,
+    int radius
+);
+
+// Cut a square image using input circle
+// 
+// Parameters:
+//   - image_in: input image
+//   - image_out: output image
+//   - top_left: cv::Point, top_left coordinate of output square in image_in
+//   - bottom_right: cv::Point, bottom_right coordinate of output square in image_in
+//   - x: circle center x
+//   - y: circle center y
+//   - radius: circle radius
+//   - padding: padding pixels to the radius (default 15)
+EXPORT_SYMBOL void cut_image_from_circle(
+    const cv::Mat& image_in,
+    cv::Mat& image_out,
+    cv::Point& top_left,
+    cv::Point& bottom_right,
+    int x,
+    int y,
+    int radius,
+    int padding         = 15
+);
 
 }
--- src/preprocessing.cpp
+++ src/preprocessing.cpp
@@ -3,8 +3,198 @@
 namespace mr
 {
 
-EXPORT_SYMBOL void resize_with_aspect_ratio(cv::Mat image, int width, int height, int longer_side_val, int inter)
+EXPORT_SYMBOL void resize_with_aspect_ratio(
+    const cv::Mat& image_in,
+    cv::Mat& image_out,
+    float& ratio_out,
+    int width,
+    int height,
+    int longer_side_val,
+    int inter
+)
 {
+    cv::Size dimension;
+    int h = image_in.size[0];
+    int w = image_in.size[1];
+    float r = 1.0;
+    
+    if (longer_side_val > 0)
+    {
+        if (h > w && h > longer_side_val)
+        {
+            height = longer_side_val;
+            width = -1;
+        }
+        else if (w >= h && w > longer_side_val)
+        {
+            height = -1;
+            width = longer_side_val;
+        }
+        else // w & h are too short
+        {
+            ratio_out = r;
+            image_in.copyTo(image_out);
+            return;
+        }
+    }
+    if (width <= 0 && height <= 0)
+    {
+        ratio_out = r;
+        image_in.copyTo(image_out);
+        return;
+    }
+    if (width <= 0)
+    {
+        r = static_cast<float>(height) / static_cast<float>(h);
+        dimension = cv::Size(static_cast<int>(w * r), static_cast<int>(height));
+    }
+    else // height is None
+    {
+        r = static_cast<float>(width) / static_cast<float>(w);
+        dimension = cv::Size(static_cast<int>(width), static_cast<int>(h * r));
+    }
+    
+    cv::resize(image_in, image_out, dimension, (0.0), (0.0), inter);
+}
+
+EXPORT_SYMBOL void apply_brightness_contrast(
+    const cv::Mat& image_in,
+    cv::Mat& image_out,
+    int brightness,
+    int contrast
+)
+{
+    int shadow = 0;
+    int highlight = 0;
+    double alpha_b = 0.0;
+    double gamma_b = 0.0;
+    double f = 0.0;
+    double alpha_c = 0.0;
+    double gamma_c = 0.0;
+    
+    if (brightness != 0)
+    {
+        if (brightness > 0)
+        {
+            shadow = brightness;
+            highlight = 255;
+        }
+        else
+        {
+            shadow = 0;
+            highlight = 255 + brightness;
+        }
+        alpha_b = (double)(highlight - shadow)/255.0;
+        gamma_b = (double)shadow;
+        
+        cv::addWeighted(image_in, alpha_b, image_in, 0, gamma_b, image_out);
+    }
+    else
+        image_in.copyTo(image_out);
+    
+    if (contrast != 0)
+    {
+        f = 131.0*((double)contrast + 127.0)/(127.0*(131.0-(double)contrast));
+        alpha_c = f;
+        gamma_c = 127.0*(1.0-f);
+        
+        cv::addWeighted(image_out, alpha_c, image_out, 0, gamma_c, image_out);
+    }
+}
+
+EXPORT_SYMBOL float calc_img_brightness_perc(
+    const cv::Mat& image_in
+)
+{
+    int pixel_sum = 0;
+    int height = image_in.size[0];
+    int width = image_in.size[1];
+    for (int x = 0; x < width; ++x)
+    {
+        for (int y = 0; y < height; ++y)
+        {
+            pixel_sum += static_cast<int>(image_in.at<uchar>(y, x));
+        }
+    }
+    
+    return static_cast<float>(
+        static_cast<float>(pixel_sum) / 255.0 / static_cast<float>(width*height)
+    );
+}
+
+EXPORT_SYMBOL float calc_circle_brightness_perc(
+    const cv::Mat& image_in,
+    int center_x,
+    int center_y,
+    int radius
+)
+{
+    int height = image_in.size[0];
+    int width = image_in.size[1];
+    
+    int xmin = center_x - radius;
+    if (xmin <= 0) xmin = 0;
+    int xmax = center_x + radius;
+    if (xmax >= width) xmax = width;
+    int ymin = center_y - radius;
+    if (ymin <= 0) ymin = 0;
+    int ymax = center_y + radius;
+    if (ymax >= height) ymax = height;
+    int radiusSquared = radius * radius;
+    
+    int pixel_sum = 0;
+    int pixel_count = 0;
+    for (int x = xmin; x < xmax; ++x)
+    {
+        for (int y = ymin; y < ymax; ++y)
+        {
+            int dx = x - center_x;
+            int dy = y - center_y;
+            int distanceSquared = dx * dx + dy * dy;
+            
+            // inside circle
+            if (distanceSquared <= radiusSquared)
+            {
+                pixel_sum += static_cast<int>(image_in.at<uchar>(y, x));
+                pixel_count += 1;
+            }
+        }
+    }
+    
+    return static_cast<float>(
+        static_cast<float>(pixel_sum) / 255.0 / static_cast<float>(width*height)
+    );
+}
+
+EXPORT_SYMBOL void cut_image_from_circle(
+    const cv::Mat& image_in,
+    cv::Mat& image_out,
+    cv::Point& top_left,
+    cv::Point& bottom_right,
+    int x,
+    int y,
+    int radius,
+    int padding
+)
+{
+    int height = image_in.size[0];
+    int width = image_in.size[1];
+    
+    int yStart = y - radius - padding;
+    if (yStart < 0) yStart = 0;
+    
+    int yEnd = y + radius + padding;
+    if (yEnd > height) yEnd = height;
+    
+    int xStart = x - radius - padding;
+    if (xStart < 0) xStart = 0;
+    
+    int xEnd = x + radius + padding;
+    if (xEnd > width) xEnd = width;
+    
+    image_out = cv::Mat(image_in, cv::Range(yStart, yEnd), cv::Range(xStart, xEnd));
+    top_left = cv::Point(xStart, yStart);
+    bottom_right = cv::Point(xEnd, yEnd);
 }
 
 }
