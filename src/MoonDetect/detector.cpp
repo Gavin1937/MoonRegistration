9d5da336a22d83dc76be0d240d5f4dbcc067f320
Gavin1937 <gyh1837771475gyh@gmail.com>
2023-11-08 19:21:28 -0800

    return c int array for c interface

--- src/MoonDetect/detector.cpp
+++ src/MoonDetect/detector.cpp
@@ -351,18 +351,28 @@ EXPORT_SYMBOL mr::Circle MoonDetector::detect_moon()
 extern "C"
 {
 
-EXPORT_SYMBOL mr::Circle detect_moon_from_filepath(const char* img_filepath)
+EXPORT_SYMBOL int* detect_moon_from_filepath(const char* img_filepath)
 {
     std::string filepath(img_filepath);
     mr::MoonDetector detector(filepath);
-    return detector.detect_moon();
+    mr::Circle circle = detector.detect_moon();
+    int* retval = (int*)malloc(3*sizeof(int));
+    retval[0] = circle.x;
+    retval[1] = circle.y;
+    retval[2] = circle.radius;
+    return retval;
 }
 
-EXPORT_SYMBOL mr::Circle detect_moon_from_binary(const unsigned char* img_binary, const int img_size)
+EXPORT_SYMBOL int* detect_moon_from_binary(const unsigned char* img_binary, const int img_size)
 {
     std::vector<unsigned char> binary(img_binary, img_binary+img_size);
     mr::MoonDetector detector(binary);
-    return detector.detect_moon();
+    mr::Circle circle = detector.detect_moon();
+    int* retval = (int*)malloc(3*sizeof(int));
+    retval[0] = circle.x;
+    retval[1] = circle.y;
+    retval[2] = circle.radius;
+    return retval;
 }
 
 }
