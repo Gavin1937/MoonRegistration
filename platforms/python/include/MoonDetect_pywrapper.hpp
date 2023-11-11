4208f10b4e418876e96f196b80fe228a0d370b30
Gavin1937 <gyh1837771475gyh@gmail.com>
2023-11-10 22:42:45 -0800

    add shapes & other members of MoonDetector

--- platforms/python/include/MoonDetect_pywrapper.hpp
+++ platforms/python/include/MoonDetect_pywrapper.hpp
@@ -10,3 +10,6 @@
 namespace py = boost::python;
 
 boost::shared_ptr<mr::MoonDetector> new_MoonDetector(const py::object& data);
+
+void wrap_MoonDetect_init_by_byte(mr::MoonDetector& self, py::object& data);
+
