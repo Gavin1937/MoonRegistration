0287a151e6c2aa2c4b9facf8e9fcdbc5a9e7b4a7
Gavin1937 <gyh1837771475gyh@gmail.com>
2023-11-11 05:20:22 -0800

    fix install, bdist, & import bugs under linux

--- platforms/python/MoonRegistration/__init__.py
+++ platforms/python/MoonRegistration/__init__.py
@@ -1,5 +1,12 @@
 import os
 from pathlib import Path
+import platform
+
+
+# os.add_dll_directory isn't available nor necessary for all platforms
+def add_dll_directory(*args):
+    if platform.system() == 'Windows':
+        os.add_dll_directory(*args)
 
 # search for dependency libraries if their environment variable exists
 # and then add their library files to python library search list
@@ -7,22 +14,24 @@ env_OpenCV_DIR = os.getenv('OpenCV_DIR')
 if env_OpenCV_DIR:
     for folder in Path(env_OpenCV_DIR).glob('**/'):
         if 'lib' in folder.name or 'bin' in folder.name:
-            os.add_dll_directory(folder)
+            add_dll_directory(folder)
 env_BOOST_ROOT = os.getenv('BOOST_ROOT')
 if env_BOOST_ROOT:
     for folder in Path(env_BOOST_ROOT).glob('**/'):
         if 'lib' in folder.name or 'bin' in folder.name:
-            os.add_dll_directory(folder)
+            add_dll_directory(folder)
 
 
 libs_dir = Path(__file__).parent / 'libs'
 if libs_dir.exists():
-    os.add_dll_directory(str(libs_dir.resolve()))
+    add_dll_directory(str(libs_dir.resolve()))
 
 
 try:
     from .__version__ import __version__
     from .libs.MoonRegistration_pywrapper import MoonDetector
-except ImportError:
-    raise ImportError('Missing Dependencies, please make sure you install all the dependencies correctly.')
+except ImportError as ierr:
+    if 'DLL' in ierr.msg:
+        raise ImportError('Missing Dependencies, please make sure you install all the dependencies correctly.')
+    raise ierr
 
