53c399d24fb9d019dc74ac7234c3a98b4d2be13c
Gavin1937 <gyh1837771475gyh@gmail.com>
2023-11-19 23:18:38 -0800

    add cv::Mat & cv2.MatLike convert & preprocessing

---
+++ platforms/python/README.md
@@ -0,0 +1,6 @@
+
+
+# Special Thanks
+
+1. [pybind11_opencv_numpy by edmBernard](https://github.com/edmBernard/pybind11_opencv_numpy) for easy conversion between cv::Mat and cv2.MatLike (np.ndarray)
+
---
+++ platforms/python/include/preprocessing_pywrapper.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <opencv2/core/mat.hpp>
+
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+
+py::tuple wrap_resize_with_aspect_ratio(
+    const cv::Mat& image_in,
+    int width,
+    int height,
+    int longer_side_val,
+    int inter
+);
+
+cv::Mat wrap_apply_brightness_contrast(
+    const cv::Mat& image_in,
+    int brightness,
+    int contrast
+);
+
+float wrap_calc_img_brightness_perc(
+    const cv::Mat& image_in
+);
+
+float wrap_calc_circle_brightness_perc(
+    const cv::Mat& image_in,
+    int center_x,
+    int center_y,
+    int radius
+);
+
+py::tuple wrap_cut_image_from_circle(
+    const cv::Mat& image_in,
+    int x,
+    int y,
+    int radius,
+    int padding
+);
