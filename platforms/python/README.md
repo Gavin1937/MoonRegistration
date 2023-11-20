c090a608cad3b77449ce5f80ce85e5d1e3f62a66
Gavin1937 <gyh1837771475gyh@gmail.com>
2023-11-20 00:57:26 -0800

    update doc

--- platforms/python/README.md
+++ platforms/python/README.md
@@ -1,4 +1,66 @@
 
+# MoonRegistration Python
+
+Python wrapper for MoonRegistration Library.
+
+# Supporting Python Version >= 3.8
+
+# Build
+
+## Build With Docker (Recommend)
+
+If you just want to try out this library, you can build it with docker using following commands.
+
+1. Under the repository root, build Docker image
+
+```sh
+docker build -t moonregistration_python -f .\Dockerfile_python .
+```
+
+2. Run Docker container
+
+```sh
+docker run -it --rm --name moonregistration_python moonregistration_python
+```
+
+1. The Docker container you just launched contains MoonRegistration library in its python environment, you can goto `demo` folder under the repository root to play with [MoonDetector_basic.py](../../demo/MoonDetector_basic.py) file. This file contains a basic example of using this library. Checkout [Usage Section for more detail](#usage)
+
+## Build Manually
+
+To build this library manually and install it to your python environment, you need to:
+
+1. Install all the dependencies and be able to [build the MoonRegistration C++ library](../../BUILDING.md#build-into-library)
+   * You need to have a C++ compiler, [CMake >= 3.11](https://cmake.org/), OpenCV, and [Python >= 3.8](https://www.python.org/)
+   * You can find install tutorial in [BUILDING.md](../../BUILDING.md)
+2. Under `platforms/python` folder, use following command to build and install this python library
+
+```sh
+python setup.py install
+```
+
+3. [Optional] You can build this library into a python .whl package, but it will be tie to your python version and cannot install in an environment with incorrect python version.
+
+```sh
+python setup.py bdist_wheel --py-limited-api cp38
+```
+
+4. [Optional] You can only build the C++ part of this library using CMake with following commands.
+
+```sh
+mkdir build
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
+cmake --build build --config=Release
+```
+
+# Usage
+
+Once you installed this library into your python environment, you can import it with:
+
+```py
+import MoonRegistration as mr
+```
+
+You can checkout [MoonDetector_basic.py](../../demo/MoonDetector_basic.py) in [demo folder](../../demo/README.md) for a simple use case.
 
 # Special Thanks
 
--- platforms/python/src/MoonRegistration_pywrapper.cpp
+++ platforms/python/src/MoonRegistration_pywrapper.cpp
@@ -25,10 +25,36 @@ std::string str_StreamOptOverload(const TYPE& obj)
 
 PYBIND11_MODULE(MoonRegistration_pywrapper, module)
 {
+    module.doc() = R"pbdoc(
+        Python wrapper for MoonRegistration Library.
+        MoonRegistration Is A Cross Platform C++ library for Moon Location Detection & Moon Image Registration.
+        --------------------
+        
+        .. currentmodule:: MoonRegistration
+        
+        .. autosummary::
+           :toctree: _generate
+        
+            version
+            Circle
+            Square
+            Rectangle
+            circle_to_square_s
+            circle_to_square_p
+            circle_to_rectangle_s
+            circle_to_rectangle_p
+            resize_with_aspect_ratio
+            apply_brightness_contrast
+            calc_img_brightness_perc
+            calc_circle_brightness_perc
+            cut_image_from_circle
+            MoonDetector
+    )pbdoc";
+    
     NDArrayConverter::init_numpy();
     
     // version.hpp
-    module.def("version", mr::version);
+    module.def("version", mr::version, R"pbdoc(MoonRegistration Library Version)pbdoc");
     
     // shapes.hpp
     py::class_<mr::Circle>(module, "Circle")
@@ -68,28 +94,93 @@ PYBIND11_MODULE(MoonRegistration_pywrapper, module)
         py::arg("width")           = -1,
         py::arg("height")          = -1,
         py::arg("longer_side_val") = -1,
-        py::arg("inter")           = static_cast<int>(cv::INTER_AREA)
+        py::arg("inter")           = static_cast<int>(cv::INTER_AREA),
+        R"pbdoc(
+    original algorithm
+    https://stackoverflow.com/a/58126805
+    
+    Parameters:
+      - image_in: input image
+      - image_out: output image
+      - ratio_out: output resize ratio
+      - width: resize image to this width (-1 to ignore)
+      - height: resize image to this height (-1 to ignore)
+      - longer_side_val: resize image's longer side to this value (-1 to ignore)
+      - inter: interpolation for cv::resize()
+        )pbdoc"
     );
     module.def("apply_brightness_contrast", &wrap_apply_brightness_contrast,
         py::arg("image_in"),
         py::arg("brightness")      = 0,
-        py::arg("contrast")        = 0
+        py::arg("contrast")        = 0,
+        R"pbdoc(
+    original algorithm
+    https://stackoverflow.com/a/50053219
+    
+    Parameters:
+      - image_in: input image
+      - image_out: output image
+      - brightness: set image brightness value (-127 to 127) (default 0)
+      - contrast: set image contrast value (-127 to 127) (default 0)
+        )pbdoc"
     );
     module.def("calc_img_brightness_perc", &wrap_calc_img_brightness_perc,
-        py::arg("image_in")
+        py::arg("image_in"),
+        R"pbdoc(
+    Assuming all pixel values in image_in are black(0) / white(255)
+    sum up all the pixels in given image, and then
+    calculate its mean. The return value represents
+    how many white pixels in the image, how bright it is.
+    
+    Parameters:
+      - image_in: input image
+    
+    Returns:
+      - float between 0 to 1
+        )pbdoc"
     );
     module.def("calc_circle_brightness_perc", &wrap_calc_circle_brightness_perc,
         py::arg("image_in"),
         py::arg("center_x"),
         py::arg("center_y"),
-        py::arg("radius")
+        py::arg("radius"),
+        R"pbdoc(
+    Assuming all pixel values in image_in are black(0) / white(255)
+    sum up all the pixels in given circle, and then
+    calculate its mean. The return value represents
+    how many white pixels in the image, how bright it is.
+    
+    part of algorithm
+    https://stackoverflow.com/a/74813748
+    
+    Parameters:
+      - image_in: input image
+      - center_x: circle center x
+      - center_y: circle center y
+      - radius: circle radius
+    
+    Returns:
+      - float between 0 to 1
+        )pbdoc"
     );
     module.def("cut_image_from_circle", &wrap_cut_image_from_circle,
         py::arg("image_in"),
         py::arg("x"),
         py::arg("y"),
         py::arg("radius"),
-        py::arg("padding")         = 15
+        py::arg("padding")         = 15,
+        R"pbdoc(
+    Cut a square image using input circle
+    
+    Parameters:
+      - image_in: input image
+      - image_out: output image
+      - rect_out: mr::Rectangle, top_left & bottom_right coordinate of output square in image_in
+      - x: circle center x
+      - y: circle center y
+      - radius: circle radius
+      - padding: padding pixels to the radius (default 15)
+        )pbdoc"
     );
     
     
@@ -100,12 +191,30 @@ PYBIND11_MODULE(MoonRegistration_pywrapper, module)
     // class MoonDetector
     py::class_<mr::MoonDetector>(module, "MoonDetector")
         .def(py::init<>())
-        .def(py::init<cv::Mat>())
-        .def(py::init(&new_MoonDetector)) // handle bytes & str param
+        .def(py::init<cv::Mat>(), R"pbdoc(
+    taking raw cv::Mat image as input
+    this constructor will assume cv_image is a decoded pixel matrix ready to use
+    it will set cv_image to original_image directly
+    colors in cv_image MUST in BGR order
+        )pbdoc")
+        // handle bytes & str param
+        .def(py::init(&new_MoonDetector), R"pbdoc(
+    Handling str image_filepath or bytes image_binary.
+    
+    Parameters:
+      - object: can be either str for filepath, or bytes for raw image bytes
+        )pbdoc")
         .def("is_empty", &mr::MoonDetector::is_empty)
         .def("init_by_path", &mr::MoonDetector::init_by_path)
         .def("init_by_byte", wrap_MoonDetect_init_by_byte)
-        .def("detect_moon", &mr::MoonDetector::detect_moon)
+        .def("detect_moon", &mr::MoonDetector::detect_moon, R"pbdoc(
+    trying to find a circle from input image
+    thats most likely contains the moon.
+    
+    Returns:
+      - if success, return mr::Circle of the circle found
+      - if fail (input doesn't contain circle), return mr::Circle of {-1, -1, -1}
+        )pbdoc")
         // add stage function pointers
     ;
 }
