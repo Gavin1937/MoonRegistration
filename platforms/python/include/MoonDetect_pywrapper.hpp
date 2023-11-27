1ad91411f9b33e01680feb0732cc20fd46ed8d37
Gavin1937 <gyh1837771475gyh@gmail.com>
2023-11-27 02:13:42 -0800

    impl set function ptr for mr.MoonDetector

--- platforms/python/include/MoonDetect_pywrapper.hpp
+++ platforms/python/include/MoonDetect_pywrapper.hpp
@@ -1,16 +1,151 @@
 #pragma once
 
+#include "ndarray_converter.h"
 #include <pybind11/pybind11.h>
+#include <pybind11/functional.h>
+#include <pybind11/numpy.h>
 namespace py = pybind11;
 
 #include <string>
 #include <vector>
 #include <memory>
+#include <functional>
 
 #include "MoonRegistration/mrapi.hpp"
 
+
+// add custom caster for std::vector<cv::Vec3f> and numpy.ndarray
+// this caster is specific to handle return value of cv::HoughCircles
+namespace pybind11 { namespace detail {
+
+template <> struct type_caster<std::vector<cv::Vec3f>> {
+public:
+    
+    PYBIND11_TYPE_CASTER(std::vector<cv::Vec3f>, _("numpy.ndarray"));
+    
+    bool load(handle src, bool /* convert */) {
+        py::array_t<float> src_array = py::cast<py::array_t<float>>(src);
+        py::buffer_info buff = src_array.request();
+        
+        // we are expecting 3d array of shape (1, x, 3)
+        // where x is the size of output vector
+        if (buff.ndim != 3)
+            return false;
+        
+        int flat_array_size = static_cast<int>(buff.size);
+        float* flat_array = static_cast<float*>(buff.ptr);
+        
+        // copy src to output value
+        value = std::vector<cv::Vec3f>(flat_array_size/3);
+        for (int counter = 0, i = 0; i < flat_array_size; i+=3, counter++)
+        {
+            value[counter] = cv::Vec3f(
+                flat_array[i+0],
+                flat_array[i+1],
+                flat_array[i+2]
+            );
+        }
+        
+        return true;
+    }
+    
+    static handle cast(const std::vector<cv::Vec3f> &src, return_value_policy, handle defval) {
+        py::array_t<float> dst_array(src.size()*3);
+        py::buffer_info dst_buff_info = dst_array.request();
+        float* dst_buff_ptr = static_cast<float*>(dst_buff_info.ptr);
+        
+        // copy src to dst
+        int idx = 0;
+        for (auto src_it : src)
+        {
+            dst_buff_ptr[idx++] = src_it[0];
+            dst_buff_ptr[idx++] = src_it[1];
+            dst_buff_ptr[idx++] = src_it[2];
+        }
+        // same as numpy.ndarray.resize, update dst_array's shape
+        dst_array.resize(py::array::ShapeContainer({1,static_cast<int>(src.size()),3}));
+        
+        handle result = handle(dst_array);
+        result.inc_ref(); // increase reference count so dst_array is visible by python
+        return result;
+    }
+};
+
+}} // namespace pybind11::detail
+
+
+// default stage functions
+
+py::tuple wrap_default_preprocess_steps(
+    const cv::Mat& image_in
+);
+
+py::tuple wrap_default_param_init(
+    const mr::ImageShape& image_shape,
+    int& max_iteration,
+    int& circle_threshold,
+    double& dp,
+    double& minDist,
+    double& minRadiusRate,
+    int& minRadius,
+    double& maxRadiusRate,
+    int& maxRadius,
+    double& param1,
+    double& param2
+);
+
+py::tuple wrap_default_iteration_param_update(
+    const int iteration,
+    const float image_brightness_perc,
+    const mr::ImageShape& image_shape,
+    int& max_iteration,
+    int& circle_threshold,
+    double& dp,
+    double& minDist,
+    double& minRadiusRate,
+    int& minRadius,
+    double& maxRadiusRate,
+    int& maxRadius,
+    double& param1,
+    double& param2
+);
+
+mr::Circle wrap_default_iteration_circle_select(
+    const int iteration,
+    const cv::Mat& image_in,
+    const std::vector<cv::Vec3f>& detected_circles
+);
+
+mr::Circle wrap_default_coordinate_remap(
+    const py::list& result_list,
+    const float resize_ratio
+);
+
+
+// class MoonDetector
 
 std::unique_ptr<mr::MoonDetector> new_MoonDetector(const py::object& data);
 
 void wrap_MoonDetect_init_by_byte(mr::MoonDetector& self, const py::bytes& data);
 
+void set_MoonDetect_preprocess_steps(mr::MoonDetector& self, std::function<py::tuple(const cv::Mat&)> func);
+
+void set_MoonDetect_param_init(
+    mr::MoonDetector& self,
+    std::function<py::tuple(const mr::ImageShape&, int, int, double, double, double, int, double, int, double, double)> func
+);
+
+void set_MoonDetect_iteration_param_update(
+    mr::MoonDetector& self,
+    std::function<py::tuple(const int, const float, const mr::ImageShape&, int, int, double, double, double, int, double, int, double, double)> func
+);
+
+void set_MoonDetect_iteration_circle_select(
+    mr::MoonDetector& self,
+    std::function<mr::Circle(const int, const cv::Mat&, const std::vector<cv::Vec3f>&)> func
+);
+
+void set_MoonDetect_coordinate_remap(
+    mr::MoonDetector& self,
+    std::function<mr::Circle(const py::list&, const float)> func
+);
--- platforms/python/src/MoonRegistration_pywrapper.cpp
+++ platforms/python/src/MoonRegistration_pywrapper.cpp
@@ -49,11 +49,14 @@ PYBIND11_MODULE(MoonRegistration_pywrapper, module)
     
     NDArrayConverter::init_numpy();
     
+    
     // version.hpp
     module.def("version", mr::version, R"pbdoc(MoonRegistration Library Version)pbdoc");
     
+    
     // shapes.hpp
     py::class_<mr::Circle>(module, "Circle")
+        .def(py::init<int,int,int>())
         .def_readwrite("x", &mr::Circle::x)
         .def_readwrite("y", &mr::Circle::y)
         .def_readwrite("radius", &mr::Circle::radius)
@@ -62,6 +65,7 @@ PYBIND11_MODULE(MoonRegistration_pywrapper, module)
     ;
     
     py::class_<mr::Square>(module, "Square")
+        .def(py::init<int,int,int>())
         .def_readwrite("x", &mr::Square::x)
         .def_readwrite("y", &mr::Square::y)
         .def_readwrite("width", &mr::Square::width)
@@ -70,6 +74,7 @@ PYBIND11_MODULE(MoonRegistration_pywrapper, module)
     ;
     
     py::class_<mr::Rectangle>(module, "Rectangle")
+        .def(py::init<int,int,int,int>())
         .def_readwrite("top_left_x", &mr::Rectangle::top_left_x)
         .def_readwrite("top_left_y", &mr::Rectangle::top_left_y)
         .def_readwrite("bottom_right_x", &mr::Rectangle::bottom_right_x)
@@ -82,6 +87,16 @@ PYBIND11_MODULE(MoonRegistration_pywrapper, module)
     module.def("circle_to_square_p", mr::circle_to_square_p);
     module.def("circle_to_rectangle_s", mr::circle_to_rectangle_s);
     module.def("circle_to_rectangle_p", mr::circle_to_rectangle_p);
+    
+    
+    // utils.hpp
+    py::class_<mr::ImageShape>(module, "ImageShape")
+        .def_readwrite("height", &mr::ImageShape::height)
+        .def_readwrite("width", &mr::ImageShape::width)
+        .def_readwrite("longer_side", &mr::ImageShape::longer_side)
+        .def_readwrite("shorter_side", &mr::ImageShape::shorter_side)
+    ;
+    module.def("calc_image_shape", mr::calc_image_shape);
     
     
     // preprocessing.hpp
@@ -183,22 +198,100 @@ PYBIND11_MODULE(MoonRegistration_pywrapper, module)
     // MoonDetect/detector.hpp
     
     // default stage functions
+    module.def("default_preprocess_steps", wrap_default_preprocess_steps, R"pbdoc(
+    Default function for preprocess_steps stage
+    
+    Parameters:
+      - image_in: cv2.MatLike|numpy.ndarray
+    
+    Returns:
+      - tuple[image_out:cv2.MatLike|numpy.ndarray, resize_ratio_out:float]
+    )pbdoc");
+    module.def("default_param_init", wrap_default_param_init, R"pbdoc(
+    Default function for param_init stage
+    
+    Parameters:
+      - image_shape: mr.ImageShape
+      - max_iteration: int
+      - circle_threshold: int
+      - dp: float
+      - minDist: float
+      - minRadiusRate: float
+      - minRadius: int
+      - maxRadiusRate: float
+      - maxRadius: int
+      - param1: float
+      - param2: float
+    
+    Returns:
+      - tuple[
+        max_iteration:int, circle_threshold:int, dp:float, minDist:float,
+        minRadiusRate:float, minRadius:int, maxRadiusRate:float, maxRadius:int,
+        param1:float, param2:float
+      ]
+    )pbdoc");
+    module.def("default_iteration_param_update", wrap_default_iteration_param_update, R"pbdoc(
+    Default function for iteration_param_update stage
+    
+    Parameters:
+      - iteration: int
+      - image_brightness_perc: float
+      - image_shape: mr.ImageShape
+      - max_iteration: int
+      - circle_threshold: int
+      - dp: float
+      - minDist: float
+      - minRadiusRate: float
+      - minRadius: int
+      - maxRadiusRate: float
+      - maxRadius: int
+      - param1: float
+      - param2: float
+    
+    Returns:
+      - tuple[
+        max_iteration:int, circle_threshold:int, dp:float, minDist:float,
+        minRadiusRate:float, minRadius:int, maxRadiusRate:float, maxRadius:int,
+        param1:float, param2:float
+      ]
+    )pbdoc");
+    module.def("default_iteration_circle_select", wrap_default_iteration_circle_select, R"pbdoc(
+    Default function for iteration_circle_select stage
+    
+    Parameters:
+      - iteration: int
+      - image_in: cv2.MatLike|numpy.ndarray
+      - detected_circles: numpy.ndarray, return value of cv2.HoughCircles
+    
+    Returns:
+      - mr.Circle
+    )pbdoc");
+    module.def("default_coordinate_remap", wrap_default_coordinate_remap, R"pbdoc(
+    Default function for coordinate_remap stage
+    
+    Parameters:
+      - result_list: list[tuple(int, mr.Circle, mr.Rectangle)]
+      - resize_ratio: float
+    
+    Returns:
+      - mr.Circle
+    )pbdoc");
     
     // class MoonDetector
     py::class_<mr::MoonDetector>(module, "MoonDetector")
         .def(py::init<>())
-        .def(py::init<cv::Mat>(), R"pbdoc(
-    taking raw cv::Mat image as input
-    this constructor will assume cv_image is a decoded pixel matrix ready to use
-    it will set cv_image to original_image directly
-    colors in cv_image MUST in BGR order
-        )pbdoc")
         // handle bytes & str param
         .def(py::init(&new_MoonDetector), R"pbdoc(
     Handling str image_filepath or bytes image_binary.
     
     Parameters:
-      - object: can be either str for filepath, or bytes for raw image bytes
+      - parameter can be one of following:
+          - str: a filepath to input image
+          - bytes: raw image bytes
+          - cv2.MatLike (numpy.ndarray), raw OpenCV image
+            this constructor will assume cv_image is a decoded pixel matrix ready to use
+            it will set cv_image to original_image directly
+            colors in cv_image MUST in BGR order
         )pbdoc")
         .def("is_empty", &mr::MoonDetector::is_empty)
         .def("init_by_path", &mr::MoonDetector::init_by_path)
@@ -211,7 +304,37 @@ PYBIND11_MODULE(MoonRegistration_pywrapper, module)
       - if success, return mr::Circle of the circle found
       - if fail (input doesn't contain circle), return mr::Circle of {-1, -1, -1}
         )pbdoc")
-        // add stage function pointers
+        // set stage function pointers
+        .def("set_preprocess_steps", set_MoonDetect_preprocess_steps, R"pbdoc(
+    Set mr::MoonDetector::preprocess_steps
+    
+    Parameters:
+      - func: callable function (cv2.MatLike|numpy.ndarray) -> tuple
+        )pbdoc")
+        .def("set_param_init", set_MoonDetect_param_init, R"pbdoc(
+    Set mr::MoonDetector::param_init
+    
+    Parameters:
+      - func: callable function (mr.ImageShape, int, int, float, float, float, int, float, int, float, float) -> tuple
+        )pbdoc")
+        .def("set_iteration_param_update", set_MoonDetect_iteration_param_update, R"pbdoc(
+    Set mr::MoonDetector::iteration_param_update
+    
+    Parameters:
+      - func: callable function (int, float, mr.ImageShape, int, int, float, float, float, int, float, int, float, float) -> tuple
+        )pbdoc")
+        .def("set_iteration_circle_select", set_MoonDetect_iteration_circle_select, R"pbdoc(
+    Set mr::MoonDetector::iteration_circle_select
+    
+    Parameters:
+      - func: callable function (int, cv2.MatLike|numpy.ndarray, numpy.ndarray) -> mr.Circle
+        )pbdoc")
+        .def("set_coordinate_remap", set_MoonDetect_coordinate_remap, R"pbdoc(
+    Set mr::MoonDetector::coordinate_remap
+    
+    Parameters:
+      - func: callable function (list, float) -> mr.Circle
+        )pbdoc")
     ;
 }
 
