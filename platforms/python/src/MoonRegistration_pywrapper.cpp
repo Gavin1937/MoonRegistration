91c70369264cfded1e7f6ed96c52dd626db75f4f
Gavin1937 <gyh1837771475gyh@gmail.com>
2024-03-01 21:34:00 -0800

    update python module descriptions

--- platforms/python/src/MoonRegistration_pywrapper.cpp
+++ platforms/python/src/MoonRegistration_pywrapper.cpp
@@ -20,19 +20,19 @@ PYBIND11_MODULE(MoonRegistration_pywrapper, module)
     module.def("version", mr::version, R"pbdoc(MoonRegistration Library Version)pbdoc");
     
     
-    py::module shapes_module = module.def_submodule("shapes", "shapes description");
+    py::module shapes_module = module.def_submodule("shapes", "Basic shape structs in the library.");
     init_shapes(shapes_module);
     
     
-    py::module imgprocess_module = module.def_submodule("imgprocess", "imgprocess description");
+    py::module imgprocess_module = module.def_submodule("imgprocess", "Basic image processing functions.");
     init_imgprocess(imgprocess_module);
     
     
-    py::module MoonDetect_module = module.def_submodule("MoonDetect", "MoonDetect description");
+    py::module MoonDetect_module = module.def_submodule("MoonDetect", "MoonDetect Module, detect moon from input image.");
     init_MoonDetect(MoonDetect_module);
     
     
-    py::module MoonRegistrate_module = module.def_submodule("MoonRegistrate", "MoonRegistrate description");
+    py::module MoonRegistrate_module = module.def_submodule("MoonRegistrate", "MoonRegistrate Module, run image registration on input image.");
     init_MoonRegistrate(MoonRegistrate_module);
     
 }
