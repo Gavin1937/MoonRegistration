590c8b7eb85b9488e62188d6bab09aba58c47dfd
Gavin1937 <gyh1837771475gyh@gmail.com>
2024-03-09 21:47:21 -0800

    setup cmake install for js

--- platforms/js/moon-registration/index.js
+++ platforms/js/moon-registration/index.js
@@ -1,36 +1,17 @@
-export {
-  // wasm_loader.js
-  instance,
-  version,
-  // image_handler.js
-  ImageHandler,
-  // shapes.js
-  Circle, Square, Rectangle,
-  circle_to_square,
-  circle_to_rectangle,
-  // imgprocess.js
-  cut_image_from_circle,
-  // MoonDetect.js
-  detect_moon,
-  // MoonRegistrate.js
-  draw_layer_image,
-  RegistrationAlgorithms,
-};
-
 import { instance } from './wasm_loader.js';
-import { ImageHandler } from './image_handler.js';
-import {
+
+export { ImageHandler } from './image_handler.js';
+export {
   Circle, Square, Rectangle,
   circle_to_square,
   circle_to_rectangle,
 } from './shapes.js';
-import { cut_image_from_circle } from './imgprocess.js';
-import { detect_moon } from './MoonDetect.js';
-import {
+export { cut_image_from_circle } from './imgprocess.js';
+export { detect_moon } from './MoonDetect.js';
+export {
   draw_layer_image,
   RegistrationAlgorithms,
 } from './MoonRegistrate.js';
-
 
 /**
  * Get MoonRegistration library version
@@ -58,3 +39,5 @@ async function version() {
     });
   });
 }
+
+export { instance, version };
